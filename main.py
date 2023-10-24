9bee262b79693eb72e363d64dc034a7180f77152
Marco Gabrecht <m.gabrecht@gmx.de>
2023-10-24 13:38:30 +0200

    ERster Tag Fom´rmular und Antwort seite

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name)
+app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def loadFile(test):
+    vokabelheft = {}
+    # Lade Datei test ein als Vokabel;lösung
+    with open(test, 'r') as f:
+        # pro zeile eine vokabel
+        for line in f:
+            # trenne vokabel und lösung
+            vokabel, losung = line.split(';')
+            vokabelheft[vokabel] = losung.strip()
+    print(vokabelheft)
+    return vokabelheft
+@app.route('/<test>')
+def test(test):
+    vokabelheft = loadFile(test)
+    return render_template('index.html', vokabeltest=vokabelheft.keys(), file=test)
+
+@app.route('/losung', methods=['POST'])
+def losung():
+    print(request.form)
+    if request.method == 'POST':
+        vokabeltest = []
+        vokabelheft = loadFile(request.form.get('file'))
+        for antwort in request.form:
+            if antwort != 'file':
+                eintrag = {'wort': antwort, 'deins': request.form.get(antwort), 'losung': vokabelheft[antwort],
+                           'ergebniss': "Falsch"}
+                if request.form.get(antwort) == vokabelheft[antwort]:
+                    print('Richtig')
+                    eintrag['ergebniss'] = "Richtig"
+                else:
+                    print('Falsch')
+                    eintrag['ergebniss'] = "Falsch"
+                vokabeltest.append(eintrag)
+        print(vokabeltest)
+        return render_template('losungsansicht.html', vokabeltest=vokabeltest)
+
+    return
 
 if __name__ == '__main__':
     app.run()
